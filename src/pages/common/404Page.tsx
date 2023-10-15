103477d65783301600a4d9b03fbc4b7c540d3423
tmquan <quantm.lqd@gmail.com>
2023-10-15 11:31:17 +0700

    Redesign 404 page

--- src/pages/common/404Page.tsx
+++ src/pages/common/404Page.tsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 import styled from "./404Page.module.scss";
 import notFound from '../../assets/404.png';
+import Color from "../../constants/Color";
 
 const NotFoundPage = () => {
   return (
     <div className={styled["container"]}>
       <div className={styled["body"]}>
         <img src={notFound} alt="404" />
-        <div>
-          Seems like this page has been lost to time. <Link to='/'>Back to front page</Link>
+        <div style={{ fontSize: '30px', color: Color.main_red_color, fontWeight: 'bold' }}>
+          Seems like this page has been lost to time.
         </div>
+        <div className={styled["text"]}><Link to='/'>Back to front page</Link></div>
       </div>
     </div>
   )
