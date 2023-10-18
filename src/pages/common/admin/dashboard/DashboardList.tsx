c75c7a91e0852787c45ccad37c72f293e070eff8
tmquan <quantm.lqd@gmail.com>
2023-10-19 01:44:14 +0700

    Handle Vietnamese word and remove word "null"

--- src/pages/common/admin/dashboard/DashboardList.tsx
+++ src/pages/common/admin/dashboard/DashboardList.tsx
@@ -4,6 +4,7 @@ import "react-circular-progressbar/dist/styles.css";
 import { UseQueryResult, useQuery } from "react-query";
 import { GetUserPagingResult, UserAPI } from "../../../../apis/UserAPI";
 import { Avatar, Spin } from "antd";
+import { removeVietnameseTones } from "../../../../utils/vietnamesefont";
 
 const DashboardList = () => {
     const {
@@ -23,7 +24,7 @@ const DashboardList = () => {
                         <div key={items.id} className={styled["data"]}>
                             <Avatar size={50} icon={<AntDesignOutlined />} src={items.imageUrl} />
                             <div>
-                                <div className={styled["username"]}>{items.fullName}</div>
+                                <div className={styled["username"]}>{removeVietnameseTones(items.fullName)}</div>
                                 <div className={styled["userdes"]}>
                                     Created On: {new Date(items.createdTime).getDate()}-{new Date(items.createdTime).getMonth() + 1}-{new Date(items.createdTime).getFullYear()}
                                 </div>
