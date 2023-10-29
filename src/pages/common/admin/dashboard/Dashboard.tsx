4362b97efab5386c4bea548aa46010bf442bec13
tmquan <quantm.lqd@gmail.com>
2023-10-29 12:13:59 +0700

    Remove unused

--- src/pages/common/admin/dashboard/Dashboard.tsx
+++ src/pages/common/admin/dashboard/Dashboard.tsx
@@ -1,58 +1,14 @@
 import styled from "./Dashboard.module.scss";
-import { Link, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Logo from "../../../../assets/milease_logo.png"
 import "react-circular-progressbar/dist/styles.css";
 import ChartRevenue from "./ChartRevenue";
 import ChartUser from "./ChartUser";
 import DashboardNumbers from "./DashboardNumbers";
 import DashboardList from "./DashboardList";
 import DashboardCircular from "./DashboardCircular";
-import jwt_decode from "jwt-decode";
-interface JWTGoogleToken {
-  name: string;
-  email: string;
-  picture: string;
-}
-
-interface JWTData {
-  sub: string,
-  id: number,
-  role: string,
-  iat?: number,
-  exp?: number,
-  traveler_status: string
-}
 
 const Dashboard = () => {
 
-  const [login, setLogin] = useState(false)
-  const navigate = useNavigate();
-
   document.title = "Dashboard"
-
-  useEffect(() => {
-    const token = localStorage.getItem("access_token")
-    const google_jwt_token = localStorage.getItem("google_jwt_token")
-    if (token && google_jwt_token) {
-      var decoded = jwt_decode<JWTGoogleToken>(google_jwt_token);
-      var decodedToken = jwt_decode<JWTData>(token);
-      if (decoded && decodedToken.role == "ADMIN") {
-        setLogin(true)
-      } else {
-        navigate('/')
-      }
-    } else {
-      setLogin(false)
-      navigate('/auth')
-    }
-  }, [])
-
-  const onLogout = () => {
-    localStorage.clear()
-    setLogin(false)
-    navigate('/auth')
-  }
 
   return (
     <>
